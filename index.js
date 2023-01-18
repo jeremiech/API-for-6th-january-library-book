41b5eeb7ed495a098894ec66e3d69e104187d387
Jeje <jeremiech06@gmail.com>
2023-01-18 15:32:38 +0200

    add my commit with correct app use cors

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 const booksRoute=require('./route/RootBook')
 const cors=require('cors')
 require("dotenv/config")
-app.use(cors())
 const port = process.env.PORT
 const uri=process.env.DB_URI
 mongoose.set("strictQuery",false)
 mongoose.connect(uri).then(()=>{
     const app = express();
+    app.use(cors())
     app.use(express.json());
     app.use('/book',booksRoute)
 
